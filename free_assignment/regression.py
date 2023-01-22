7499bfd3b546ba2e1b5b712d9e3715d1c5db0d4c
siida36 <siida0118@gmail.com>
2023-01-22 23:08:34 +0900

    Add comment

--- free_assignment/regression.py
+++ free_assignment/regression.py
@@ -1,3 +1,12 @@
+"""Pythonによる重回帰分析のサンプルです。
+
+次の流れで処理を実行します。
+
+1. データ読み込み
+2. 重回帰分析
+3. 結果の表示
+"""
+from pathlib import Path
 from typing import Dict
 
 from sklearn.linear_model import LinearRegression
@@ -6,6 +15,7 @@ import pandas as pd
 
 
 def read_data_dummy() -> Dict[str, np.array]:
+    """デバッグ用のダミーデータです"""
     X = np.array([[1, 2, 3], [4, 5, 6]])
     y = np.array([0, 1])
     return {
@@ -14,11 +24,13 @@ def read_data_dummy() -> Dict[str, np.array]:
     }
 
 
-def read_data_from_csv() -> Dict[str, np.array]:
-    df = pd.read_csv("input/input_std.csv")
+def read_data_from_csv(filepath: Path) -> Dict[str, np.array]:
+    """ファイルからデータを読み込みます"""
+    df = pd.read_csv(filepath)
     y = df["label"].to_numpy()
     del df["label"]
     X = df.to_numpy()
+
     return {
         "X": normalize(X),
         "y": normalize(y)
@@ -26,6 +38,7 @@ def read_data_from_csv() -> Dict[str, np.array]:
 
 
 def normalize(matrix: np.array) -> np.array:
+    """標準化処理を行います"""
     mean = matrix.mean(axis=0)
     diff = (matrix - mean) ** 2
     std = np.sqrt(diff.mean())
@@ -34,18 +47,21 @@ def normalize(matrix: np.array) -> np.array:
 
 
 def train(input_data) -> LinearRegression:
+    """重回帰分析を行います"""
     model = LinearRegression()
     model.fit(input_data["X"], input_data["y"])
     return model
 
 
 def print_params(model: LinearRegression):
-    print(f"coef: {model.coef_}")
-    print(f"bias: {model.intercept_}")
+    """重回帰分析の結果を表示します"""
+    print(f"係数: {model.coef_}")
+    print(f"切片: {model.intercept_}")
 
 
 def main():
-    input_data = read_data_from_csv()
+    """すべての処理を実行します"""
+    input_data = read_data_from_csv("input/input_std.csv")
     model = train(input_data)
     print_params(model)
 
